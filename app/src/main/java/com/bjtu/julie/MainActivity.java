e5c9eb3e4db6ad9be64b631bcda8f247bd8ff5a7
jyhe <15301144@bjtu.edu.cn>
2018-03-24 16:10:18 +0800

    try by He

--- app/src/main/java/com/bjtu/julie/MainActivity.java
+++ app/src/main/java/com/bjtu/julie/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a=0;int b=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
